6133f4113e3937e25854f8acff2ef67618adb7a9
Cecilia Pasquale <cecilia.pasquale16@gmail.com>
2019-07-03 10:10:57 -0400

    first and second test success

--- src/game.js
+++ src/game.js
@@ -1,17 +1,21 @@
 //Begin the Game
 // Throws are when the player attempts to knock down pins, players get 2 throws per frame unless they throw a strike
 // if a player throws a strike the score for the frame is 10 plus the total pins knocked down on the next two throws
-
+//Empty Array for player throws to be stored in
 var Game = function () {
-
+    this.throw = [];
     
 };
 
-Game.prototype.throws = function () {
-    
+Game.prototype.throws = function (pins) {
+    this.throw.push(pins)
 
 };
-
+//There are 10 Frames but players get 2 throws per frame
 Game.prototype.playerScore = function () {
-    return "you need lessons";
+    var score = 0
+    for (var i = 0; i < 20; i++){
+        score += this.throw[i]
+    }
+    return score;
 }
--- test/gameTest.js
+++ test/gameTest.js
@@ -7,10 +7,17 @@ describe("Game", function () {
         for (var i = 0; i < 20; i++){
             game.throws(0)
         }
-        expect(game.playerScore()).equal("you need lessons");
+        expect(game.playerScore()).equal(0);
     }
 );
-   
+    it("can roll a game of twos", function () {
+        var game = new Game();
+        for (var i = 0; i < 20; i++) {
+            game.throws(2)
+        }
+        expect(game.playerScore()).equal(40);
 
+    })
+    
 });
 
