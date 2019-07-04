7369b1b10f24ec714336b3d16bd7ad53371a86ad
Cecilia Pasquale <cecilia.pasquale16@gmail.com>
2019-07-04 12:45:34 -0400

    all tests pass

--- src/game.js
+++ src/game.js
@@ -1,36 +1,45 @@
-//Begin the Game
 // Throws are when the player attempts to knock down pins, players get 2 throws per frame unless they throw a strike
 // if a player throws a strike the score for the frame is 10 plus the total pins knocked down on the next two throws
-//if a plaler throws a spare the score for the frame is 10 plus the number of pins knocked down on the next throw
+//if a player throws a spare the score for the frame is 10 plus the number of pins knocked down on the next throw
 
 var Game = function () {
     this.throw = [];
-    
 };
 
 Game.prototype.throws = function (pins) {
     this.throw.push(pins)
 
 };
-//There are 10 Frames but players get 2 throws per frame
-//Needs to work for Spares and Strikes
+
 Game.prototype.playerScore = function () {
     var score = 0;
     var throwInFrame = 0;
     var game = this;
     for (var frame = 0; frame < 10; frame++) {
 
-
-        if (spare()) {
+        if (strike()) {
+            score += strikeScore();
+            throwInFrame++;
+            
+        }
+        else if (spare()) {
             score += spareScore();
+            throwInFrame += 2;
         }
         else {
             score += noBonusScore();
+                throwInFrame += 2;
         }
-      
-        throwInFrame += 2;
+    
     }
     return score;
+
+    function strike() {
+        return game.throw[throwInFrame] == 10;
+    }
+    function strikeScore() {
+        return game.throw[throwInFrame] + game.throw[throwInFrame + 1] + game.throw[throwInFrame + 2];
+    }
 
     function spare() {
         return game.throw[throwInFrame] + game.throw[throwInFrame + 1] == 10;
--- test/gameTest.js
+++ test/gameTest.js
@@ -1,6 +1,4 @@
 //refactor to simplify
-
-
 describe("Game", function () {
 
     var game;
@@ -13,10 +11,18 @@ describe("Game", function () {
         throwGame(0, 20);
         expect(game.playerScore()).equal(0);
     }
-);
+    );
+    
     it("can score a game of sixes", function () {
-        throwGame(3, 20);
+        throwGame(3, 20)
         expect(game.playerScore()).equal(60);
+
+    });
+
+    it("can score a game of nines", function () {
+ // 4.5 to show  a thrown combination of 9 pins a frame
+        throwGame(4.5, 20);
+        expect(game.playerScore()).equal(90);
 
     });
 
@@ -24,17 +30,41 @@ describe("Game", function () {
         game.throws(5)
         game.throws(5)
         game.throws(4)
-        game.throws(2);
-        throwGame(0,16)
+        game.throws(2)
+        throwGame(0, 16)
         expect(game.playerScore()).equal(20)
-    })
+    });
 
+    it("can score of all spares", function () {
+        throwGame(5, 20)
+        game.throws(5)
+        expect(game.playerScore()).equal(150)
+    });
+    it("can score a game with a strike bonus", function () {
+        game.throws(10)
+        game.throws(5)
+        game.throws(4)
+        throwGame(0, 17)
+        expect(game.playerScore()).equal(28)
+    });
 
-    function throwGame(pins, throws ) {
+    it("can score a game with strike bonus at end of game", function () {
+        throwGame(4.5, 18)
+        game.throws(10)
+        game.throws(2)
+        game.throws(3)
+        expect(game.playerScore()).equal(96)
+
+    });
+
+    it("can score a perfect game", function () {
+        throwGame(10, 12)
+        expect(game.playerScore()).equal(300)
+    });
+
+    function throwGame(pins, throws) {
         for (var i = 0; i< throws; i++) {
-           game.throws(pins);
-            
+           game.throws(pins);  
         }
     }
-
 });
